a869455fd56d8885ac95c1c5a758a800911851ea
Supreetha <supreetha.bhat@walmart.com>
2020-05-19 01:53:23 +0530

    Updates

---
+++ precache-manifest.fc3abbc467724c0cf8fe4124f1a7fad2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d58180c683739f0c7b438715beee68",
+    "url": "/React_MyTask/index.html"
+  },
+  {
+    "revision": "475f1229182d092f7ba5",
+    "url": "/React_MyTask/static/css/main.1491be4f.chunk.css"
+  },
+  {
+    "revision": "08a43c2d4e5b582602fc",
+    "url": "/React_MyTask/static/js/2.bc56352c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_MyTask/static/js/2.bc56352c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "475f1229182d092f7ba5",
+    "url": "/React_MyTask/static/js/main.24d0a2ce.chunk.js"
+  },
+  {
+    "revision": "68ba00519094f8f5bb8a",
+    "url": "/React_MyTask/static/js/runtime-main.991c2314.js"
+  },
+  {
+    "revision": "251a0c3d1a7a119838fa3c830e53aea0",
+    "url": "/React_MyTask/static/media/Supreetha.251a0c3d.png"
+  },
+  {
+    "revision": "806db2c44eaa5e0b528b1097cb1e9216",
+    "url": "/React_MyTask/static/media/pen-solid.806db2c4.svg"
+  },
+  {
+    "revision": "2cbc2c36cd8fc72d60e6ed1add7c4d1e",
+    "url": "/React_MyTask/static/media/search-solid.2cbc2c36.svg"
+  },
+  {
+    "revision": "bda6265448ab94e28214889df10afdaa",
+    "url": "/React_MyTask/static/media/trash-solid.bda62654.svg"
+  }
+]);
