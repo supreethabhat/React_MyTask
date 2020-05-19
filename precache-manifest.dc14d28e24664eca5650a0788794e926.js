86c11d1e9efe904eeb2bb0741a397956b555b519
Supreetha <supreetha.bhat@walmart.com>
2020-05-20 02:03:44 +0530

    Updates

---
+++ precache-manifest.dc14d28e24664eca5650a0788794e926.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5ad60c9b3ffe5d35292d2fb24960cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "14175b3780cb06d188b4",
+    "url": "./static/css/main.6b6a5375.chunk.css"
+  },
+  {
+    "revision": "a170c144e7fa401f8408",
+    "url": "./static/js/2.792bcea3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.792bcea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14175b3780cb06d188b4",
+    "url": "./static/js/main.bc88080b.chunk.js"
+  },
+  {
+    "revision": "983f2ed069c1fb04d852",
+    "url": "./static/js/runtime-main.1b601902.js"
+  },
+  {
+    "revision": "251a0c3d1a7a119838fa3c830e53aea0",
+    "url": "./static/media/Supreetha.251a0c3d.png"
+  },
+  {
+    "revision": "6e24e3b978f0871e2b1a6c4a9e887577",
+    "url": "./static/media/close-outline.6e24e3b9.svg"
+  },
+  {
+    "revision": "806db2c44eaa5e0b528b1097cb1e9216",
+    "url": "./static/media/pen-solid.806db2c4.svg"
+  },
+  {
+    "revision": "2cbc2c36cd8fc72d60e6ed1add7c4d1e",
+    "url": "./static/media/search-solid.2cbc2c36.svg"
+  },
+  {
+    "revision": "bda6265448ab94e28214889df10afdaa",
+    "url": "./static/media/trash-solid.bda62654.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React_MyTask/precache-manifest.fc3abbc467724c0cf8fe4124f1a7fad2.js"
+  "./precache-manifest.dc14d28e24664eca5650a0788794e926.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_MyTask/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
